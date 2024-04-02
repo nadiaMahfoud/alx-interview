c8e88d518f160eedfb24d549639ce20cb4bf0b1c
nadia mahfoud <nadiamahfoud48@gmail.com>
2024-04-02 15:46:51 +0000

    0. Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,9 +6,10 @@ This is the main file used for testing the makeChange function.
 
 def makeChange(coins, amount):
     """
-    Calculates the number of coins of each type needed to make the change for the given amount.
-    It iterates through the list of coins, deducting the maximum possible number of each coin from the amount.
-    If it's not possible to make the change, it returns -1.
+    Calculates the number of coins of each type needed to make the change for
+    the given amount. It iterates through the list of coins, deducting the
+    maximum possible number of each coin from the amount. If it's not possible
+    to make the change, it returns -1.
     """
     if amount < 1:
         return 0
@@ -21,4 +22,3 @@ def makeChange(coins, amount):
         amount -= num * coin
         count += num
     return count if amount == 0 else -1
-
